3a7732b47c5b483a6796f1143df1cae1fc457863
Mateus Paixão <mateus.borges.paixao@gmail.com>
2024-09-11 22:08:32 -0300

    Aula 1: Primeiros passos com TS

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('Oi eu sou TS');
+function ola(nome) {
+    return "Hello, " + nome;
+}
+console.log(ola("1234"));
---
+++ script.ts
@@ -0,0 +1,8 @@
+console.log('Oi eu sou TS');
+
+function ola(nome: string) {
+    return "Hello, " + nome;
+}
+
+
+console.log(ola("1234"));
